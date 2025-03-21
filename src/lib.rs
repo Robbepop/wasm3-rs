603bdea32718cd4195a2af0f74d4bb47e441476a
Robin Freyler <robin.freyler@gmail.com>
2025-03-21 15:21:36 +0100

    remove unused macro export

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ pub use self::environment::Environment;
 mod function;
 pub use self::function::{CallContext, Function, RawCall};
 mod macros;
-pub use self::macros::*;
 mod module;
 pub use self::module::{Module, ParsedModule};
 mod runtime;
